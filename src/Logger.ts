d07d258dd7d4b732067189e9ec3e2585a7e04174
Adam Shumann <ashumann13@gmail.com>
2017-06-22 22:33:07 -0500

    Visual logging... Just because.

--- src/Logger.ts
+++ src/Logger.ts
@@ -12,6 +12,9 @@ export const styles = {
   [LogLevel.FATAL]:  "color: yellow; background-color: red",
 };
 
+let y = 0;
+let tick = 0;
+
 export class Logger implements IPosisLogger {
   private prefix: string;
   public level: LogLevel = LogLevel.INFO;
@@ -27,7 +30,17 @@ export class Logger implements IPosisLogger {
       }
       let style = styles[level] || styles.default;
       console.log(`<log severity="${level}" style="${style}">[${level}] ${this.prefix} ${message}</log>`);
+      this.vlog(level, `[${level}] ${this.prefix} ${message}`)
     }
+  }
+  private vlog(level: LogLevel, message: string): void {
+    if (tick != Game.time) y = 0.2
+    tick = Game.time
+    let style = styles[level] || styles.default;
+    let color = style.match(/color: ([a-z]*)/)[1]
+    let vis = new RoomVisual()
+    vis.text(message, 0, y, { align: 'left', color })
+    y += 0.8
   }
   debug (message: (() => string) | string): void {
     this.log(LogLevel.DEBUG, message);
