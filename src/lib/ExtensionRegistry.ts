57214a082132221c70521cae52462b878028bf2d
Adam Shumann <ashumann13@gmail.com>
2017-06-30 12:08:40 -0500

    [ExtensionRegistry] Added unregister

--- src/lib/ExtensionRegistry.ts
+++ src/lib/ExtensionRegistry.ts
@@ -3,9 +3,10 @@ import { Logger } from "../lib/Logger";
 let logger = new Logger("[ExtensionRegistry]");
 logger.level = LogLevel.DEBUG;
 
-export class ExtensionRegistry {
+export class ExtensionRegistry implements IPosisExtension {
   private registry: { [interfaceId: string]: IPosisExtension } = {};
   constructor() {
+    this.register('agsExtensionRegistry', this);
   }
   register(interfaceId: string, extension: IPosisExtension): boolean {
     if (this.registry[interfaceId]) {
@@ -15,6 +16,16 @@ export class ExtensionRegistry {
     logger.debug(`Registered ${interfaceId}`);
     this.registry[interfaceId] = extension;
     return true;
+  }
+  unregister(interfaceId: string): boolean {
+   if (this.registry[interfaceId]) {
+      logger.debug(`Unregistered ${interfaceId}`);
+      delete this.registry[interfaceId]
+      return true; 
+    } else {
+      logger.error(`Interface Id not registered: ${interfaceId}`);
+      return false;
+    }
   }
   getExtension(interfaceId: string): IPosisExtension | undefined {
     if (!this.registry[interfaceId]) return;
